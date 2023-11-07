24a75f7ac56c64ac7ec5909dcea1034e55798d45
mgfahem <mgfahem@gmail.com>
2023-11-07 16:02:04 +0100

    my first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+
+    return (
+        <>
+            <h1>Créer votre playlist et partagez la !</h1>
+        </>
+    )
+}
+
+export default Header;
