6afa3b0c18a4f470f79bf02c693433437373308d
mgfahem <mgfahem@gmail.com>
2023-11-10 14:50:13 +0100

    fix: App.jsx issues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,8 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import HomePage from './components/HomePage';
 import Login from './components/Login';
-import './styles/homepage.css';
-import './styles/login.css'
+import './assets/styles/homepage.css';
+import './assets/styles/login.css'
 
 function App() {
   return (
