4c8123afe8ab24e30f785233e63ba17f634a2731
Nyoote <120457719+Nyoote@users.noreply.github.com>
2023-11-10 13:07:56 +0100

    fix: searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,66 +1,105 @@
 import React from 'react';
 import { ReactSearchAutocomplete } from 'react-search-autocomplete';
 import { useState } from "react";
+import '../styles/searchbar.css'
 
 
 function SearchBar() {
-    const items = [
-        {
-          id: 0,
-          name: 'CSS'
-        },
-        {
-          id: 1,
-          name: 'JavaScript'
-        },
-        {
-          id: 2,
-          name: 'Basic'
-        },
-        {
-          id: 3,
-          name: 'PHP'
-        },
-        {
-          id: 4,
-          name: 'Java'
-        }
-    ]
+  const [array, setArray] = useState([]);
+  const [items, setItems] = useState([]);
+  const [searchValue, setSearchValue] = useState([]);
 
     let playlist = []
 
-    const [array, setArray] = useState([]);
-    const addItem = () => {
-        array.push('aaaaa');
-        playlist.push(array);
-        setArray(playlist);
+      const handleSearch = (value) => {
+        setSearchValue(value)
       };
-
-      
-      const formatResult = (item) => {
+  
+      const handleOnSelect = (item) => {
+        let isUnique = true
+        
+        array.forEach((element) => {
+          if (element.id == item.id){
+            isUnique = false
+          }
+        })
+        if (isUnique){
+          playlist.push(
+            item
+        )
+          setArray(array.concat(playlist))
+        }
+      }
+      const DataPlaylist = ({data}) => {
+        const dataArray = []
         return (
           <>
-            <span>{item.name}</span>
+          <div className='dataPlaylist'>
+            {data.map(item => (
+            <div key={item.id}>
+              <p>Name: {item.name}</p>
+            </div>
+        ))}
+          </div>
           </>
+        )
+      } 
+
+      const sendRequest = () => {
+        let dataSearchbar = searchValue
+        const token = 'BQCfVhAjhw5TAPmOW-n65cubtXOoRP31CSRwbIFL6u0_ZlpTMt2mZo5Eziejk3owtChcCxlHV3CdEtzYwWPRKr0W82MNcIt45JwCgCgBJxHIoyaj2o0'
+        const url = `https://api.spotify.com/v1/search?q=${dataSearchbar}&type=track`
+        fetch(url, {
+          methode: 'GET',
+          headers: {
+            Authorization: `Bearer ${token}`
+          },
+        })
+        .then(
+          response => {
+            if (!response.ok) {
+              throw new Error('http error status :' + response.status)
+            }
+            return response.json()
+          }
+        )
+        .then(
+          data => {
+            let extractedData = data.tracks.items.map(item => ({
+              id: item.id,
+              name: item.name,
+              artist: item.artists[0].name
+            }))
+            setItems(extractedData)
+            console.log('extractedData', extractedData)
+            return extractedData
+        })
+        .catch(
+          error => {
+            console.error(error.message)
+          }
         )
       }
     
       return (
-        <div style={{display:'flex', justifyContent: 'center'}}>
-
-            <div style={{ width: 400}}>
+        <div className='searchbarComponent'>
+            <div className='searchbar'>
               <ReactSearchAutocomplete
                 items={items}
-                autoFocus
-                formatResult={formatResult}
+                onSelect={handleOnSelect}
+                onSearch={handleSearch}
               />
-              <div>
-                {array}
-              </div>
-                <button onClick={addItem}>Add song</button>
+              <div className="playlistComponent">
+              <div className='arrayComponent'>
+                <DataPlaylist
+                data = {array}
+                />
+              </div>  
+            </div>
+            <button onClick={sendRequest}>Send</button>
             </div>
             </div>
       )
 }
 
-export default SearchBar
+export default SearchBar 
