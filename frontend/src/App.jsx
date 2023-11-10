da461b5d2f9880c9882a763a1cec9319a135a364
anirbas2 <sabrina.attos@gmail.com>
2023-11-10 01:18:28 +0100

    page Login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,12 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import HomePage from './components/HomePage';
 import './styles/homepage.css';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <HomePage />
-      <Footer />
+    <div>
+      <Login/>
     </div>
   );
 }
